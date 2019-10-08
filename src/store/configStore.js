7113916042926ecd70abbe035c48c06e24b7e846
Leochens <18332518328@163.com>
2019-10-08 15:17:05 +0800

    redux-devtool

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,10 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
-const logger = createLogger();
+import { composeWithDevTools } from 'redux-devtools-extension';
+const logger = createLogger({ collapsed: true });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
-
+// const store = createStore(rootReducer, applyMiddleware(logger));
+// const store = createStore(rootReducer, composeWithDevTools());
+//非侵入式
+let store = process.env.NODE_ENV === 'production' ? (
+    createStore(rootReducer, applyMiddleware(logger))
+) : (
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? (
+            createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__())
+        ) : (
+                createStore(rootReducer, applyMiddleware(logger))
+            )
+    )
 export default store;
 
 
